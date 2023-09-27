0aeb75708f8fa120b7afce27d1d0323709b03186
kaancpapi <kaanc0754@gmail.com>
2023-09-27 11:15:02 +0300

    Initial commit

---
+++ calculatorApp/calculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorAppApp.swift
+//  calculatorApp
+//
+//  Created by Kaan Çakır on 27.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct calculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
